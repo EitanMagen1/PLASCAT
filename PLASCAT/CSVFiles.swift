00d9cc9e3fb24a31e568b9ec4e5ee1ae53c6847e
EitanMagen1 <eitan.magen1@gmail.com>
2016-02-05 10:42:37 +0200

    CSwiftV
    https://github.com/Daniel1of1/CSwiftV

--- PLASCAT/CSVFiles.swift
+++ PLASCAT/CSVFiles.swift
@@ -10,28 +10,31 @@ import UIKit
 
 class CSVFiles {
     static let sheredInstance = CSVFiles()
+    var error: NSErrorPointer = nil
 
-    var csv:CSV
-    
-    init() {
-        let csvURL = NSBundle(forClass: CSVFiles.self).URLForResource("PLS_LUL", withExtension: "csv")
-        csv = CSV(contentsOfFile: String(csvURL), error: nil)!
-        //example
-        getItemForItemNumber("02001025")
-    }
-    
-    
-    func getItemForItemNumber(itemNum: String)-> Dictionary<String, String>{
-        let item:Dictionary<String, String> = Dictionary<String, String>()
-
-        for row in csv.rows {
-            if row["Item Number"] == itemNum {
-                print("\(row)")
-                return row
-            }
-        }
+    func getItemForItemNumber(searchText : String){
         
-        return item
+        
+        let csvURL = NSBundle(forClass: CSVFiles.self).URLForResource("PLS_LUL", withExtension: "csv")
+        let inputString = "Year,Make,Model,Description,Price\r\n1997,Ford,E350,descrition,3000.00\r\n1999,Chevy,Venture,another description,4900.00\r\n"
+        
+        let csv = CSwiftV(String: inputString)
+        
+        let rows = csv.rows
+        print("\(rows)")
+        //[
+        //  ["1997","Ford","E350","descrition","3000.00"],
+        //  ["1999","Chevy","Venture","another description","4900.00"]
+        // ]
+        
+        let headers = csv.headers // ["Year","Make","Model","Description","Price"]
+        
+        let keyedRows = csv.keyedRows // [
+        //  ["Year":"1997","Make":"Ford","Model":"E350","Description":"descrition","Price":"3000.00"],
+        //  ["Year":"1999","Make":"Chevy","Model":"Venture","Description":"another, description","Price":"4900.00"]
+        // ]
+
     }
-    
 }
+
+
