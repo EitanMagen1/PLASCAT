026e3b8eea00e344207828db9b0b8676f8f9ff7d
EitanMagen1 <eitan.magen1@gmail.com>
2016-02-05 10:59:55 +0200

    description

--- PLASCAT/CSVFiles.swift
+++ PLASCAT/CSVFiles.swift
@@ -13,12 +13,15 @@ class CSVFiles {
     var error: NSErrorPointer = nil
 
     func getItemForItemNumber(searchText : String){
-        
-        
+        //working url to the file
         let csvURL = NSBundle(forClass: CSVFiles.self).URLForResource("PLS_LUL", withExtension: "csv")
+        //missing a way to 
+        //
+        //test string seperated by commas
         let inputString = "Year,Make,Model,Description,Price\r\n1997,Ford,E350,descrition,3000.00\r\n1999,Chevy,Venture,another description,4900.00\r\n"
         
         let csv = CSwiftV(String: inputString)
+        
         
         let rows = csv.rows
         print("\(rows)")
